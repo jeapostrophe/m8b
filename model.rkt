9334fb6efe0e03055f0abc062ba79d01a2e810df
Jay McCarthy <jay.mccarthy@gmail.com>
2013-06-15 08:56:15 -0600

    stxmap

--- model.rkt
+++ model.rkt
@@ -2,22 +2,23 @@
 (require (prefix-in 19: srfi/19)
          (planet jaymccarthy/mongodb)
          (for-syntax unstable/syntax
+                     syntax/stx
                      racket/syntax))
 
 (define (call-with-model thunk)
   (define mongo-server #f)
   (define root-db #f)
-  (dynamic-wind 
-   (λ ()
-     (set! mongo-server (create-mongo))
-     (if root-db
-         (set-mongo-db-mongo! root-db mongo-server)
-         (set! root-db (make-mongo-db mongo-server "m8b"))))
-   (λ ()
-     (parameterize ([current-mongo-db root-db])
-       (thunk)))
-   (λ ()
-     (close-mongo! mongo-server))))
+  (dynamic-wind
+      (λ ()
+        (set! mongo-server (create-mongo))
+        (if root-db
+          (set-mongo-db-mongo! root-db mongo-server)
+          (set! root-db (make-mongo-db mongo-server "m8b"))))
+      (λ ()
+        (parameterize ([current-mongo-db root-db])
+          (thunk)))
+      (λ ()
+        (close-mongo! mongo-server))))
 
 (define-mongo-struct
   applicant "applicants"
@@ -41,27 +42,27 @@
    [toefl]
    [research-area]
    [advisor]
-   ; Files
+                                        ; Files
    [pdf-application] ; apple-script / button_delete_green
    [pdf-letters] ; document_blank / button_delete_blue
    [pdf-transcript] ; curriculum_vitae / button_delete_red
-   ; Metadata
+                                        ; Metadata
    [comments #:push #:required]
    [decisions #:push #:pull #:required]
    [tags #:set-add #:pull #:required]))
 
 (define (null+ l r)
   (if (or (bson-null? l) (bson-null? r))
-      bson-null
-      (+ l r)))
+    bson-null
+    (+ l r)))
 (define (null< l r)
   (if (or (bson-null? l) (bson-null? r))
-      #f
-      (< l r)))
+    #f
+    (< l r)))
 (define (null-time<? l r)
   (if (or (bson-null? l) (bson-null? r))
-      #f
-      (19:time<? l r)))
+    #f
+    (19:time<? l r)))
 
 (define (applicant-raw-gre a)
   (null+ (applicant-gre-verbal-score a)
@@ -76,8 +77,8 @@
 
 (define (bson-default x def)
   (if (bson-null? x)
-      def
-      x))
+    def
+    x))
 (define (applicant-research-area* a)
   (bson-default (applicant-research-area a) ""))
 (define (applicant-advisor* a)
@@ -89,23 +90,23 @@
   (syntax-case stx ()
     [(_ base suf ...)
      (quasisyntax/loc stx
-       (list #,@(syntax-map (lambda (i)
-                              (format-id #'base "~a~a" #'base i))
-                            #'(suf ...))))]))
+       (list #,@(stx-map (lambda (i)
+                           (format-id #'base "~a~a" #'base i))
+                         #'(suf ...))))]))
 
 (define (applicant-complete? a)
-  (empty? 
+  (empty?
    (filter (compose bson-null? (lambda (f) (f a)))
            (id-list applicant-
-                    citizenship lds? financial-aid? 
-                    gre-date 
+                    citizenship lds? financial-aid?
+                    gre-date
                     gre-verbal-score gre-verbal-percentile
                     gre-quant-score gre-quant-percentile
                     gre-analytic-score gre-analytic-percentile
                     prior-school cumulative-gpa degree degree-sought
                     pdf-application pdf-letters pdf-transcript))))
 
-; XXX [toefl-okay? (symbols 'waived 'cleared 'not-cleared)]
+                                        ; XXX [toefl-okay? (symbols 'waived 'cleared 'not-cleared)]
 
 (define (applicants)
   (mongo-dict-query "applicants" empty))
